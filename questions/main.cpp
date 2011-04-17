39a10009efd5686cd4542db628b02d16ce79c201
Damian Blakeman <dee@The-Machine.(none)>
2011-04-17 23:45:00 +0100

    working program   it asks players name then says hello followed by first name

---
+++ questions/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+//what is the players name?
+
+    string fullname, fname, mname, lname;
+
+    cout<<"please enter full name\n\n";
+    cin>>fname>>mname>>lname;
+
+    fullname= fname+" "+mname+" "+lname;
+    cout<<fullname<<"\n\n";
+
+
+//greeting player
+
+        cout<<"Hello "<<fname<<"\n";
+
+
+
+
+
+
+
+
+
+    return 0;
+}
