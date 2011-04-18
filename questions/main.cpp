6e4173ab1b3f9442eda6336a74d244071052924a
Damian Blakeman <dee@The-Machine.(none)>
2011-04-18 01:11:42 +0100

    asking five questions and then reprintin onto screen with very tidy code

--- questions/main.cpp
+++ questions/main.cpp
@@ -18,37 +18,59 @@ int main()
 
 //greeting player
 
-    cout<<"Hello "<<fname<<"\n";
+    string greeting;
+    greeting="Hello "+fname+"\n";
+    cout<< greeting;
 
 //define answers
     string answer1,answer2,answer3,answer4,answer5;
 //define multiple choice
-    cout<<"Please enter a,b,c,d,e\n a)Excellent\n b)Good\n c)Ok\n d)Fair\n e)Shit\n\n";
+    string multiple_choice_answers;
+    cout<< "Please enter a,b,c,d,e\n";
+    multiple_choice_answers= "a)Excellent\n b)Good\n c)Ok\n d)Fair\n e)Shit\n\n";
+
+
+//define the questions
+
+
+    string question1,question2,question3,question4,question5;
+    question1="How are you?\n";
+    question2="how is the weather?\n";
+    question3="How would you rate Liyha??\n";
+    question4="Do you like Tim?\n";
+    question5="What is your age?\n";
+
+
 
 //question 1
-    cout<<"How are you?\n";
+
+
+    cout<<question1;
+    cout<<multiple_choice_answers;
     cin>>answer1;
 
 
 //question 2
 
-    cout<<"how is the weather?\n";
+    cout<<question2;
+    cout<<multiple_choice_answers;
     cin>>answer2;
 
 //question 3
 
-    cout<<"How would you rate Liyha??\n";
+
+    cout<<question3;
     cin>>answer3;
 
 //question 4
 
-    cout<<"Do you like Tim?\n";
+    cout<<question4;
     cin>>answer4;
 
 
 //question 5
 
-    cout<<"What is your age?\n";
+    cout<<question5;
     cin>>answer5;
 
 
