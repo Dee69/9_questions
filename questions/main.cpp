849090e780490258c2d093308cad5bcb1ef9a981
Damian Blakeman <dee@The-Machine.(none)>
2011-04-18 18:07:00 +0100

    9 Questions, reprints input to user, very clean code, fully separated

--- questions/main.cpp
+++ questions/main.cpp
@@ -11,83 +11,115 @@ int main()
 //questions in a list, etc...
 
 
+//defining what is the players name?
+
+    string fullname, fname, mname, lname,enter_full_name_instuc;
+
+    enter_full_name_instuc="please enter full name\n\n";
+
+//    fullname= fname+" "+mname+" "+lname+"\n\n";
 
 
 
+//defining the multiple questions answers
 
+    string A1, A2, A3, A4, A5, A6, A7, A8, A9;
 
-//what is the players name?
+//defining multiple choice
 
-    string fullname, fname, mname, lname;
-
-    cout<<"please enter full name\n\n";
-    cin>>fname>>mname>>lname;
-
-    fullname= fname+" "+mname+" "+lname;
-//    cout<<fullname<<"\n\n";
-
-
-//greeting player
-
-    string greeting;
-    greeting="Hello "+fname+"\n";
-    cout<< greeting;
-
-//define answers
-    string answer1,answer2,answer3,answer4,answer5;
-//define multiple choice
     string multiple_choice_answers;
-    cout<< "Please enter a,b,c,d,e\n";
     multiple_choice_answers= " a)Excellent\n b)Good\n c)Ok\n d)Fair\n e)Shit\n\n";
-
 
 //define the questions
 
+    string Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9;
 
-    string question1,question2,question3,question4,question5;
-    question1="How are you?\n";
-    question2="how is the weather?\n";
-    question3="How would you rate Liyha??\n";
-    question4="Do you like Tim?\n";
-    question5="What is your age?\n";
+    Q1="How are you?\n\n";
+    Q2="How is the weather?\n\n";
+    Q3="How would you rate Liyha??\n\n";
+    Q4="Do you like Tim?\n\n";
+    Q5="What is your age?\n\n";
+    Q6="What time is it?\n";
+    Q7="Are you working tommorrow?\n\n";
+    Q8="How do you feel about working there?\n\n";
+    Q9="How long have you worked there?\n\n";
+
+//defining the instructions
+
+    string instruc;
+    instruc="\n\n Please enter a,b,c,d,e\n\n";
+
+//what is the players name?
+
+    cout<<enter_full_name_instuc;
+    cin>>fname>>mname>>lname;
+    fullname= fname+" "+mname+" "+lname+"\n\n";
+    cout<<fullname;
+    cout<<"Hello "+fname;
+
+//greeting player
+
+   // cout<< greeting;
+
+
+//giving instructions to player
+
+    cout<< instruc;
 
 
 
 //question 1
 
 
-    cout<<question1;
+    cout<<Q1;
     cout<<multiple_choice_answers;
-    cin>>answer1;
+    cin>>A1;
 
 
 //question 2
 
-    cout<<question2;
+    cout<<Q2;
     cout<<multiple_choice_answers;
-    cin>>answer2;
+    cin>>A2;
 
 //question 3
 
 
-    cout<<question3;
-    cin>>answer3;
+    cout<<Q3;
+    cout<<multiple_choice_answers;
+    cin>>A3;
 
 //question 4
 
-    cout<<question4;
-    cin>>answer4;
+    cout<<Q4;
+    cout<<multiple_choice_answers;
+    cin>>A4;
 
 
 //question 5
 
-    cout<<question5;
-    cin>>answer5;
+    cout<<Q5;
+    cin>>A5;
 
+//question 6
 
+    cout<<Q6;
+    cin>>A6;
 
+//question 7
 
+    cout<<Q7;
+    cin>>A7;
 
+//question 8
+
+    cout<<Q8;
+    cin>>A8;
+
+//question 9
+
+    cout<<Q9;
+    cin>>A9;
 
 
 
@@ -100,16 +132,15 @@ int main()
 // So print answers..
 
 
-        cout<<answer1;
-        cout<<answer2;
-        cout<<answer3;
-        cout<<answer4;
-        cout<<answer5;
-
-
-
-
-
+        cout<<"Answer for Question 1) -\t"+A1+"\n";
+        cout<<"Answer for Question 2) -\t"+A2+"\n";
+        cout<<"Answer for Question 3) -\t"+A3+"\n";
+        cout<<"Answer for Question 4) -\t"+A4+"\n";
+        cout<<"Answer for Question 5) -\t"+A5+"\n";
+        cout<<"Answer for Question 6) -\t"+A6+"\n";
+        cout<<"Answer for Question 7) -\t"+A7+"\n";
+        cout<<"Answer for Question 8) -\t"+A8+"\n";
+        cout<<"Answer for Question 9) -\t"+A9+"\n";
 
 
     return 0;
