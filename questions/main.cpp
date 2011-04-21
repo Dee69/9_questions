a6b98ac4a03834cce97505fdc26eba9f8656e9e7
Peter David Hamilton <peter@hamilton.name>
2011-04-21 12:23:57 +0100

    pointer being freed was not allocated

--- questions/main.cpp
+++ questions/main.cpp
@@ -3,96 +3,58 @@
 #include "main.h"
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+
+// define instructions
+    string greeting;
+    greeting="Hello ";
+    string instruc;
+    instruc="\n\n Please enter a,b,c,d,e:\n\n";
+    string multiple_choice_answers;
+    multiple_choice_answers=" a)Excellent\n b)Good\n c)Ok\n d)Fair\n e)Shit\n\n";
 
 // define player
     string fullname, fname, mname, lname,enter_full_name_instuc;
     enter_full_name_instuc="please enter full name\n\n";
 
-// define questions
-    string Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9;
-    Q1="How are you?\n\n";
-    Q2="How is the weather?\n\n";
-    Q3="How would you rate Liyha??\n\n";
-    Q4="Do you like Tim?\n\n";
-    Q5="What is your age?\n\n";
-    Q6="What time is it?\n";
-    Q7="Are you working tommorrow?\n\n";
-    Q8="How do you feel about working there?\n\n";
-    Q9="How long have you worked there?\n\n";
-
-// define answers
-    string A1, A2, A3, A4, A5, A6, A7, A8, A9;
-
-// define instructions
-    string greeting;
-    greeting="Hello ";
-    string multiple_choice_answers;
-    multiple_choice_answers=" a)Excellent\n b)Good\n c)Ok\n d)Fair\n e)Shit\n\n";
-    string instruc;
-    instruc="\n\n Please enter a,b,c,d,e:\n\n";
-
 // what is the players name?
     cout<<enter_full_name_instuc;
     cin>>fname>>mname>>lname;
-    fullname= fname+" "+mname+" "+lname+"";
+    fullname=fname+" "+mname+" "+lname;
     cout<<greeting<<fname;
+    cout<<instruc;
 
-// giving instructions to player
-    cout<< instruc;
+// define questions
+    string questions[8];
+    questions[0]="How are you?\n\n";
+    questions[1]="How is the weather?\n\n";
+    questions[2]="How would you rate .... ??\n\n";
+    questions[3]="Do you like .... ?\n\n";
+    questions[4]="What is your age?\n\n";
+    questions[5]="What time is it?\n";
+    questions[6]="Are you working ... ?\n\n";
+    questions[7]="How do you feel about working there?\n\n";
+    questions[8]="How long have you worked there?\n\n";
 
-// question 1
-    cout<<Q1;
-    cout<<multiple_choice_answers;
-    cin>>A1;
+// define answers
+    string answers[8];
 
-// question 2
-    cout<<Q2;
-    cout<<multiple_choice_answers;
-    cin>>A2;
+// ask questions
+    for(int i=0; i<=8; i++) {
+        cout<<questions[i];
+        cout<<multiple_choice_answers;
+        cin>>answers[i];
+    }
 
-// question 3
-    cout<<Q3;
-    cout<<multiple_choice_answers;
-    cin>>A3;
-
-// question 4
-    cout<<Q4;
-    cout<<multiple_choice_answers;
-    cin>>A4;
-
-// question 5
-    cout<<Q5;
-    cin>>A5;
-
-// question 6
-    cout<<Q6;
-    cin>>A6;
-
-// question 7
-    cout<<Q7;
-    cin>>A7;
-
-// question 8
-    cout<<Q8;
-    cin>>A8;
-
-// question 9
-    cout<<Q9;
-    cin>>A9;
-
-// print answers
-
-int i=1;
-while(i<10,i++) {
-    cout<<") -\t A"<<i<<"\n";
-}
-
-
-// save to file
-    ofstream answers("/tmp/9_questions.csv", ios::app);
-    answers<<fullname<<","<<A1<<","<<A2<<","<<A3<<","<<A4<<","<<A5<<","<<A6<<","<<A7<<","<<A8<<","<<A9<<"\n";
+// print and save answers
+    ofstream save_answers("/tmp/9_questions.csv", ios::app);
+    save_answers<<fullname;
+    for(int i=0; i<=8; i++) {
+        cout<<"A"<<i<<": "<<answers[i]<<"\n";
+        save_answers<<","<<answers[i];
+    }
+    save_answers<<"\n";
 
 // END
     return 0;
